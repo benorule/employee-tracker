e8c3ee1891535ac579419e49e843fa40b3e24941
Benjamin Turnbull <benorule@outlook.com>
2020-08-06 17:27:04 +0930

    add SQL queries to schema file

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -1,13 +1,11 @@
 DROP DATABASE IF EXISTS employeesDB;
 CREATE database employeesDB;
-
 USE employeesDB;
 
-CREATE TABLE employees (
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(role_id)
+CREATE TABLE departments (
+	department_id INT NOT NULL,
+	name VARCHAR(30),
+    PRIMARY KEY (department_id)
 );
 
 CREATE TABLE roles (
@@ -19,11 +17,20 @@ CREATE TABLE roles (
     FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
     
-CREATE TABLE departments (
-	department_id INT NOT NULL,
-	name VARCHAR(30),
-    PRIMARY KEY (department_id)
+CREATE TABLE employees (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
+
+INSERT INTO departments (department_id, name)
+VALUES (1, "IT");
+
+INSERT INTO departments (department_id, name)
+VALUES (2, "HR");
 
 INSERT INTO roles (role_id, title, salary, department_id)
 VALUES (1, "Junior Developer", 65.5, 1);
@@ -34,8 +41,22 @@ VALUES (2, "Senior Developer", 85.5, 1);
 INSERT INTO roles (role_id, title, salary, department_id)
 VALUES (3, "Manager", 75.5, 2);
 
-INSERT INTO departments (department_id, name)
-VALUES (1, "IT");
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (1, "Barry", "Allen", 1);
 
-INSERT INTO departments (department_id, name)
-VALUES (2, "HR");
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (2, "Peter", "Parker", 1);
+
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (3, "Tony", "Stark", 2);
+
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (4, "Bruce", "Wayne", 2);
+
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (5, "Steve", "Rogers", 3);
+
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (6, "Clark", "Kent", 3);
+
+SELECT * FROM employees;
